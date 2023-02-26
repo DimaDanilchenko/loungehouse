f2f3c74476486be6a3eeb0dcc5bf7f4dcb7b5d7c
Dima Danilchenko <switch57dima@yandex.ru>
2023-02-26 17:06:48 +0300

    123

--- js/script.js
+++
@@ -1,5 +0,0 @@
-const body = document.querySelector('.body');
-const menuButton = body.querySelector('.hamburger-menu');
-menuButton.addEventListener('click', function(){
-  body.classList.toggle('overflow-hidden');
-})
